9278d9293864c21eb7224c21181788e1d96684de
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-05-31 18:04:46 +0900

    feat: add footer

---
+++ global-top.vue
@@ -0,0 +1,9 @@
+<!-- hide the footer from "cover" layout -->
+<template>
+  <footer
+    v-if="$nav.currentLayout !== 'cover'"
+  >
+    <Scroll/>
+  <div class="absolute bottom-5 left-0 right-5 text-right text-xs"><SlideCurrentNo/>/<SlidesTotal/></div>
+  </footer>
+</template>
