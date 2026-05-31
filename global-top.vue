e7b9d57753fcf8c3fd4c62200461d7ed76436969
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-06-01 00:13:17 +0900

    fix: no slide number on first slide

--- global-top.vue
+++ global-top.vue
@@ -1,7 +1,7 @@
 <!-- hide the footer from "cover" layout -->
 <template>
   <footer
-    v-if="$nav.currentLayout !== 'cover'"
+    v-if="$nav.currentLayout !== 'cover' && $nav.currentLayout !== 'image'"
   >
     <Scroll/>
   <div class="absolute bottom-5 left-0 right-5 text-right text-xs"><SlideCurrentNo/>/<SlidesTotal/></div>
